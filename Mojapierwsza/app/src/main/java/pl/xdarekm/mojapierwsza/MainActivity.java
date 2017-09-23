df4b4032f65cacfecf80d1a269451b6460c5fd41
unknown <darekmarciniak69@gmail.com>
2017-09-23 14:15:19 +0200

    zadanie3,4

--- Mojapierwsza/app/src/main/java/pl/xdarekm/mojapierwsza/MainActivity.java
+++ Mojapierwsza/app/src/main/java/pl/xdarekm/mojapierwsza/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.zadanie2);
+        setContentView(R.layout.zadanie4);
     }
 }
 
