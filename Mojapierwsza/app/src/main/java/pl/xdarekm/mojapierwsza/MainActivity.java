a02f5fb149796c3cda5a12106fcd1531aa399178
unknown <darekmarciniak69@gmail.com>
2017-09-24 08:52:03 +0200

    zad7

--- Mojapierwsza/app/src/main/java/pl/xdarekm/mojapierwsza/MainActivity.java
+++ Mojapierwsza/app/src/main/java/pl/xdarekm/mojapierwsza/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.zad6);
+        setContentView(R.layout.zad7);
     }
 }
 
